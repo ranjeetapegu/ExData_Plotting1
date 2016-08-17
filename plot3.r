6dfc58416eeb86c49ad20b9835fc66037bad4a0a
Ranjeeta <ranjeetapegu@gmail.com>
2016-08-17 15:11:20 -0400

    code did not comited properly

--- plot3.r
+++ plot3.r
@@ -15,8 +15,9 @@ nrow(h) ## 2880
 datetime = paste(h$Date, h$Time)
 h$datetime<-as.POSIXct(datetime)
 #plot 3
-
+png(file="Plot3.png", width=480, height=480)
 plot(h$Sub_metering_1~h$datetime, type="l",ylab="Energy sub metering", xlab="")
 lines(h$Sub_metering_2~h$datetime,col ="red")
 lines(h$Sub_metering_3~h$datetime,col="blue")
 legend("topright", col = c("black","red","blue"), lty=1, lwd=2, legend = c("Sub_mertering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
